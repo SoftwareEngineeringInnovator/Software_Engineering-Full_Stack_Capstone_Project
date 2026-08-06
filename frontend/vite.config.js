99a7a36e275e3510463aaeeaa7b3bbbd4863e9ae
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-06 11:26:03 -0400

    connected backend localhost with front end

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // Forward API requests from React to the Express backend
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
