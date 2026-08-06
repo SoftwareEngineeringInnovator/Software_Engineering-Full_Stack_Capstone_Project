1b546a411dd49f50d90f566ce5a816584794105a
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-06 14:31:08 -0400

    created POST route, succesfully tested error handling for the POST route

--- backend/routes/incidentRoutes.js
+++ backend/routes/incidentRoutes.js
@@ -6,16 +6,39 @@ const router = express.Router();
 
 // GET route /api/incidents
 router.get("/", async (req, res) => {
-  try {
-    const incidents = await Incident.find();
+    try {
+        const incidents = await Incident.find();
 
-    res.status(200).json(incidents);
-  } catch (error) {
-    res.status(500).json({
-      message: "Unable to retrieve incidents",
-      error: error.message,
-    });
-  }
+        res.status(200).json(incidents);
+    } catch (error) {
+        res.status(500).json({
+            message: "Unable to retrieve incidents",
+            error: error.message,
+        });
+    }
+});
+
+// POST route /api/incidents - create an incident in MongoDB
+router.post("/", async (req, res) => {
+    try {
+        const newIncident = await Incident.create(req.body);
+
+        res.status(201).json(newIncident);
+    } catch (error) {
+        // Return a 400 response when incident data does not pass validation
+        if (error.name === "ValidationError") {
+            return res.status(400).json({
+                message: "Incident validation failed",
+                error: error.message,
+            });
+        }
+        
+        // Display error message 
+        res.status(500).json({
+            message: "Unable to create incident",
+            error: error.message,
+        });
+    }
 });
 
 export default router;
