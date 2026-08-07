e268e1a14b8d3eab013a780b017ad972d46ffee9
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-07 13:14:00 -0400

    added comments to the Navigation.jsx file for reference

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router";
 
+// This component provides shared navigation links for the application
 function Navigation() {
   return (
     <nav>
+      {/* Link to the Home page */}
       <Link to="/">Home</Link>
+
+      {/* Link to the incidents page */}
       <Link to="/incidents">Incidents</Link>
     </nav>
   );
 }
 
+// Export the component so it can be used in App.jsx
 export default Navigation;
