3ad31f39aec9c51924c0677ad22e60a8f66c166c
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-07 11:00:01 -0400

    added shared Navigation component to  App.jsx file so each page will have a navigation menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router";
+import Navigation from "./components/Navigation.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import IncidentsPage from "./pages/IncidentsPage.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/incidents" element={<IncidentsPage />} />
-    </Routes>
+    <>
+      <Navigation />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/incidents" element={<IncidentsPage />} />
+      </Routes>
+    </>
   );
 }
 
