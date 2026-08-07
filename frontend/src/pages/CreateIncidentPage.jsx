6ebcc66ac680ac7717c33f24d3fb7596abf82a9c
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-07 13:08:46 -0400

    create CreateIncidentPage for reporting new incidents

---
+++ frontend/src/pages/CreateIncidentPage.jsx
@@ -0,0 +1,15 @@
+// This page will allow users to POST/submit a new incident ticket.
+function CreateIncidentPage() {
+  return (
+    <main>
+      {/* Heading incident page */}
+      <h1>Report New Incident</h1>
+
+      {/* Explaining the purpose of this page */}
+      <p>Use this page to report a new cybersecurity incident.</p>
+    </main>
+  );
+}
+
+// Export the page so it can be used by React Router in App.jsx
+export default CreateIncidentPage;
