fff9c673dbdd065e8c0160024dacdc3835829b86
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-07 10:06:47 -0400

    added HomePage and IncidentsPage routes to App.jsx page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
 import IncidentsPage from "./pages/IncidentsPage.jsx";
 
 function App() {
-  return <IncidentsPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/incidents" element={<IncidentsPage />} />
+    </Routes>
+  );
 }
 
 export default App;
