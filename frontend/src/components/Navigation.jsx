4ab709687d0a480af8fe23fe1d27e2e29f372853
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-07 13:31:55 -0400

    added the link to the Navigation component for the page where users can report an incident

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -2,15 +2,18 @@ import { Link } from "react-router";
 
 // This component provides shared navigation links for the application
 function Navigation() {
-  return (
-    <nav>
-      {/* Link to the Home page */}
-      <Link to="/">Home</Link>
+    return (
+        <nav>
+            {/* Link to the Home page */}
+            <Link to="/">Home</Link>
 
-      {/* Link to the incidents page */}
-      <Link to="/incidents">Incidents</Link>
-    </nav>
-  );
+            {/* Link to the incidents page */}
+            <Link to="/incidents">Incidents</Link>
+
+            {/* Link to the page where users report the incident */}
+            <Link to="/incidents/new">Report Incident</Link>
+        </nav>
+    );
 }
 
 // Export the component so it can be used in App.jsx
