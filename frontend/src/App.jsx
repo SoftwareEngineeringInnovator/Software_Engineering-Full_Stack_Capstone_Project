297ab8effa8e040c96dfb25bd43f97ddc407e0f9
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-07 23:49:47 -0400

    added the route for the Incident Details page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import IncidentsPage from "./pages/IncidentsPage.jsx";
 import CreateIncidentPage from "./pages/CreateIncidentPage.jsx";
+import IncidentDetailsPage from "./pages/IncidentDetailsPage.jsx";
 
 function App() {
   return (
@@ -10,9 +11,17 @@ function App() {
       <Navigation />
 
       <Routes>
+        {/* Displays the Home page */}
         <Route path="/" element={<HomePage />} />
+
+        {/* Displays the page that lists the reported cybersecurity incidents */}
         <Route path="/incidents" element={<IncidentsPage />} />
+
+        {/* Displays the form where users can report a cybersecurity incident */}
         <Route path="/incidents/new" element={<CreateIncidentPage />} />
+
+        {/* Displays one incident based on the ID */}
+        <Route path="/incidents/:id" element={<IncidentDetailsPage />} />
       </Routes>
     </>
   );
