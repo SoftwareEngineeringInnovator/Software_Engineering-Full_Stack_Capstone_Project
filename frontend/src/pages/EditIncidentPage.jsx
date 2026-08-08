2220ebbd10ec1039238ef3f71a41a0fbf3b3e3b2
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-08 04:00:18 -0400

    created EditIncidentPage component for updating an incident

---
+++ frontend/src/pages/EditIncidentPage.jsx
@@ -0,0 +1,15 @@
+// This page will allow users to update an existing cybersecurity incident
+function EditIncidentPage() {
+  return (
+    <main>
+      {/* Page heading for editing an existing incident */}
+      <h1>Edit Incident</h1>
+
+      {/* Paragrah to explain the action allowed */}
+      <p>Update the information for the selected cybersecurity incident.</p>
+    </main>
+  );
+}
+
+// Export the page so it can be used by React Router in App.jsx
+export default EditIncidentPage;
