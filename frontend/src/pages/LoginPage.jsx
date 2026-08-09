26f956f98fdcf0f8fdd22ff5ba2828ccdce44d17
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-09 14:02:35 -0400

    created Login page for user authentication

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,14 @@
+// This page will allow registered users to log in to the application
+function LoginPage() {
+  return (
+    <main>
+      {/* Page heading for user authentication */}
+      <h1>Login</h1>
+
+      <p>Log in to access the Cybersecurity Incident Tracker.</p>
+    </main>
+  );
+}
+
+// Export the page so it can be used by React Router
+export default LoginPage;
