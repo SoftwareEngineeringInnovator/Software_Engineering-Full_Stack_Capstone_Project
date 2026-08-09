3675a4e4fe49c3655c8872ed7fefddff20cc585c
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-09 04:12:56 -0400

    created authentication routes file

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import User from "../models/User.js";
+
+// Create a router for authentication
+const router = express.Router();
+
+// Export the router so it can be connected to the Express application
+export default router;
