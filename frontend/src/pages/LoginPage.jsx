82567456a813031395a03b351f0b4fafcc76c008
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-09 14:15:14 -0400

    added form state for user login credentials, successfully tested using Inspect tool

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
+
 // This page will allow registered users to log in to the application
 function LoginPage() {
-  return (
-    <main>
-      {/* Page heading for user authentication */}
-      <h1>Login</h1>
 
-      <p>Log in to access the Cybersecurity Incident Tracker.</p>
-    </main>
-  );
+    // Stores the email and password entered in the login page
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    // Added console.log for testing purposes
+    console.log("Login form state:", formData);
+
+    return (
+        <main>
+            {/* Page heading for user authentication */}
+            <h1>Login</h1>
+
+            <p>Log in to access the Cybersecurity Incident Tracker.</p>
+        </main>
+    );
 }
 
 // Export the page so it can be used by React Router
