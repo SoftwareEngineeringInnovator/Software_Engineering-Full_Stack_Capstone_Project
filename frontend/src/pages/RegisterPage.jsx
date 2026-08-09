0bf5c643c4450d36ee18ccf4f533c61979a37a04
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-08-09 16:21:07 -0400

    added form state for new user registration, successfully tested using Inspect tool

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -3,14 +3,28 @@ import { useNavigate } from "react-router";
 
 // This page will allow new users to create an account
 function RegisterPage() {
-  return (
-    <main>
-      {/* Page heading for new user registration */}
-      <h1>Register</h1>
 
-      <p>Create an account to access the Cybersecurity Incident Tracker.</p>
-    </main>
-  );
+    // Store the information entered by the new user
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    // Added console.log for testing purposes
+    console.log("Register form state:", formData);
+
+    return (
+        <main>
+            {/* Page heading for new user registration */}
+            <h1>Register</h1>
+
+            <p>Create an account to access the Cybersecurity Incident Tracker.</p>
+        </main>
+    );
+
+    // Added console.log for testing purposes
+
 }
 
 // Export the page so it can be used by React Router
